fd2c4f80beab96d78b8146ebf80457e7446d29c1
Connereg <connereg@optonline.net>
2020-11-05 19:25:27 -0600

    Second Commit: Page Object Model

---
+++ obKLesson9.js
@@ -0,0 +1,134 @@
+module.exports = {
+    url: 'https://www.yoodlize.com/',
+    elements: {
+        seeallButton1: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton2: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton3: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton4: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton5: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton6: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[6]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton7: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[7]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton8: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[8]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton9: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[9]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton10: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[10]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton11: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[11]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton12: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[12]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton13: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[13]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton14: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[14]',
+            locateStrategy: 'xpath'
+        },
+        seeallButton15: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[15]',
+            locateStrategy: 'xpath'
+        
+        },
+
+    checkCatagory: {
+        selector: '(//div[@class="sc-jKVCRD jSqgxr"])',
+        locateStrategy: 'xpath'
+        },
+    
+        bottomscreenLink1: {
+        selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[1]',
+        locateStrategy: 'xpath'
+        },
+        bottomscreenLink2: {
+        selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[2]',
+        locateStrategy: 'xpath'
+        },
+        bottomscreenLink3: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[3]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink4: {
+        selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[4]',
+        locateStrategy: 'xpath'
+        },
+        bottomscreenLink5: {
+        selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[5]',
+        locateStrategy: 'xpath'
+        },
+        bottomscreenLink6: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[6]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink7: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[7]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink8: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[8]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink9: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[9]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink10: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[10]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink11: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[11]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink12: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[12]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink13: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[13]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink14: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[14]',
+            locateStrategy: 'xpath'
+        },
+        bottomscreenLink15: {
+            selector:  '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[16]',
+            locateStrategy: 'xpath'
+        }
+
+    
+    }
+}
